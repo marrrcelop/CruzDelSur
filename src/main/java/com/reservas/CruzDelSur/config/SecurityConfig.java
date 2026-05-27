2021c4a3bd657e738daee18afef49b694448612a
Tu Nombre <tuemail@example.com>
2026-05-27 09:18:25 -0500

    Agregamos Aministrador

--- src/main/java/com/reservas/CruzDelSur/config/SecurityConfig.java
+++ src/main/java/com/reservas/CruzDelSur/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.reservas.CruzDelSur.config;
 
+import com.reservas.CruzDelSur.entity.Usuario;
+import com.reservas.CruzDelSur.repository.UsuarioRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +30,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/usuarios/**", "/usuarios").permitAll() // <--- AGREGA ESTA LÍNEA
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -31,13 +39,25 @@ public class SecurityConfig {
         return http.build();
     }
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("admin") // El usuario que pusiste en Postman
-                .password("{noop}123") // {noop} le dice a Spring que la contraseña no está encriptada por ahora
-                .roles("USER")
-                .build();
+    public UserDetailsService userDetailsService(UsuarioRepository repository) {
+        return username -> { // El 'username' que llega aquí será el correo
+            Usuario usuario = repository.findByCorreo(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
 
-        return new InMemoryUserDetailsManager(user);
+            return User.builder()
+                    .username(usuario.getCorreo())
+                    .password(usuario.getContrasena_hash()) // Spring validará el hash automáticamente
+                    .roles(usuario.getRol().equals("Administrador") ? "ADMIN" : "USER")
+                    .build();
+        };
+    }
+    // 1. Configuramos BCrypt para encriptar las contraseñas
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
